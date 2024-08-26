38d7120a1b269d0953830a8fe122d3fccdf2c5e5
iceet <iceet>
2024-08-26 21:09:55 +0800

    update

---
+++ src/components/Icons/assets/TimerCountdownUtilityOutlined.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { IconProps } from '../type';
+import Icon from '@ant-design/icons';
+import iconsvg from './svg/outlined/timer-countdown-utility.svg';
+
+export default class TimerCountdownUtilityOutlined extends React.Component<IconProps> {
+    public render() {
+        let  props: any = this.props;
+        return <Icon component={iconsvg} {...props} />
+    }
+}
